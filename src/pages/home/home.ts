6df05ef5940f7eabf33409310f7729f011ab8992
fadilah <fadilahsaadah.wahab@me-tech.com.my>
2018-04-01 10:26:44 +0800

    add function camera

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ActionSheetController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
@@ -8,7 +9,7 @@ import { ActionSheetController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public actionSheetCtrl: ActionSheetController) {
+  constructor(public navCtrl: NavController, public actionSheetCtrl: ActionSheetController, private camera: Camera) {
 
   }
 
@@ -37,5 +38,22 @@ export class HomePage {
       ]
     });
     actionSheet.present();
+  }
+
+  takePicture(){
+  	 const options: CameraOptions = {
+	  quality: 100,
+	  destinationType: this.camera.DestinationType.DATA_URL,
+	  encodingType: this.camera.EncodingType.JPEG,
+	  mediaType: this.camera.MediaType.PICTURE
+	}
+
+	this.camera.getPicture(options).then((imageData) => {
+	 // imageData is either a base64 encoded string or a file URI
+	 // If it's base64:
+	 let base64Image = 'data:image/jpeg;base64,' + imageData;
+	}, (err) => {
+	 // Handle error
+	});
   }
 }
